cda02898d0422a77f10d395559614f9de48dd88b
lovelease <lovelease@gmail.com>
2013-04-01 14:51:02 +0800

    first commit test

--- gen/com/example/helloandroid/R.java
+++ gen/com/example/helloandroid/R.java
@@ -15,10 +15,12 @@ public final class R {
     }
     public static final class id {
         public static final int btn=0x7f070002;
-        public static final int menu_settings=0x7f070004;
+        public static final int btn_goNext=0x7f070005;
+        public static final int menu_settings=0x7f070006;
         public static final int nameTxt=0x7f070001;
         public static final int showContent=0x7f070003;
         public static final int txtView=0x7f070000;
+        public static final int txtView_hintMsg=0x7f070004;
     }
     public static final class layout {
         public static final int activity_intent=0x7f030000;
@@ -30,6 +32,7 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int goNextBtn=0x7f040007;
         public static final int hello_world=0x7f040001;
         public static final int hintStr=0x7f040003;
         public static final int menu_settings=0x7f040002;
