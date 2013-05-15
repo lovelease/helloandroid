de824a34a5c9c6a2d663f1c04e262b7dad1fbdf2
lovelease <lovelease@gmail.com>
2013-05-15 16:28:43 +0800

    添加地图缩放控件，地图显示方式

--- src/com/example/helloandroid/GoogleMapActivity.java
+++ src/com/example/helloandroid/GoogleMapActivity.java
@@ -4,13 +4,33 @@ import android.os.Bundle;
 import android.view.Menu;
 
 import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapController;
+import com.google.android.maps.MapView;
 
 public class GoogleMapActivity extends MapActivity {
 
+	MapView mapView;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_google_map);
+		mapView = (MapView) findViewById(R.id.mapView);
+		
+		// 设置地图显示类型：卫星，地图，街道（默认设置为街道模式）
+		mapView.setStreetView(true);
+		// 卫星模式
+//		mapView.setSatellite(true);
+		// 是否显示交通信息
+//		mapView.setTraffic(true);
+		
+		
+		// 添加地图缩放控件
+		mapView.setBuiltInZoomControls( true );
+		// 控制缩放比例
+		MapController mapController = mapView.getController();
+		// 设置 16 档为当前缩放大小(google map共21档）（具体缩放效果见：http://greg-koppel.site88.net/maps/InfoDisplay.html）
+		mapController.setZoom( 16 );
 	}
 
 	@Override
