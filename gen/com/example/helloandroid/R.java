488b01c47980705465148326b483c3881dcbd1a8
lovelease <lovelease@gmail.com>
2013-05-21 18:07:23 +0800

    add dropdown activity to be the launch activity

--- gen/com/example/helloandroid/R.java
+++ gen/com/example/helloandroid/R.java
@@ -24,33 +24,38 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000e;
+        public static final int action_settings=0x7f080010;
         public static final int bmapsView=0x7f080000;
-        public static final int btn=0x7f080005;
-        public static final int btn_baidumap=0x7f08000c;
-        public static final int btn_goNext=0x7f080008;
-        public static final int btn_googlemap=0x7f08000a;
-        public static final int mapView=0x7f080001;
-        public static final int menu_settings=0x7f08000d;
-        public static final int nameTxt=0x7f080004;
-        public static final int showContent=0x7f080006;
-        public static final int showMsgFromPrePage=0x7f080002;
-        public static final int txtView=0x7f080003;
-        public static final int txtView_hintMsg=0x7f080007;
-        public static final int txtView_hintMsg_baidumap=0x7f08000b;
-        public static final int txtView_hintMsg_googlemap=0x7f080009;
+        public static final int btn=0x7f080006;
+        public static final int btn_baidumap=0x7f08000d;
+        public static final int btn_goNext=0x7f080009;
+        public static final int btn_googlemap=0x7f08000b;
+        public static final int container=0x7f080001;
+        public static final int mapView=0x7f080002;
+        public static final int menu_settings=0x7f08000f;
+        public static final int nameTxt=0x7f080005;
+        public static final int section_label=0x7f08000e;
+        public static final int showContent=0x7f080007;
+        public static final int showMsgFromPrePage=0x7f080003;
+        public static final int txtView=0x7f080004;
+        public static final int txtView_hintMsg=0x7f080008;
+        public static final int txtView_hintMsg_baidumap=0x7f08000c;
+        public static final int txtView_hintMsg_googlemap=0x7f08000a;
     }
     public static final class layout {
         public static final int activity_baidu_map=0x7f030000;
-        public static final int activity_google_map=0x7f030001;
-        public static final int activity_intent=0x7f030002;
-        public static final int activity_main=0x7f030003;
+        public static final int activity_basic=0x7f030001;
+        public static final int activity_google_map=0x7f030002;
+        public static final int activity_intent=0x7f030003;
+        public static final int activity_main=0x7f030004;
+        public static final int fragment_basic_dummy=0x7f030005;
     }
     public static final class menu {
         public static final int activity_intent=0x7f070000;
         public static final int activity_main=0x7f070001;
         public static final int baidu_map=0x7f070002;
-        public static final int google_map=0x7f070003;
+        public static final int basic=0x7f070003;
+        public static final int google_map=0x7f070004;
     }
     public static final class string {
         public static final int action_settings=0x7f05000d;
@@ -71,6 +76,7 @@ public final class R {
         /**  for BaiduMapActivity 
          */
         public static final int title_activity_baidu_map=0x7f050010;
+        public static final int title_activity_basic=0x7f050012;
         /**  for GoogleMapActivity 
          */
         public static final int title_activity_google_map=0x7f05000f;
@@ -78,6 +84,9 @@ public final class R {
          */
         public static final int title_activity_intent=0x7f05000e;
         public static final int title_activity_main=0x7f050006;
+        public static final int title_section1=0x7f050013;
+        public static final int title_section2=0x7f050014;
+        public static final int title_section3=0x7f050015;
         public static final int welcome=0x7f050005;
     }
     public static final class style {
